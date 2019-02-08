7436ffd138d0215c174aef0a9e4840517d8d0351
paxan102 <paxan2012@ya.ru>
2019-02-08 20:19:56 +0300

    AddProject

---
+++ PushOverTest/Controllers/RequestController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Specialized;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace PushOverTest.Controllers
+{
+    //[Produces("application/json")]
+    [Route("api/[controller]")]
+    public class RequestController : Controller
+    {
+        [HttpPost]
+        public void Post(string token, string user, string message)
+        {
+            var parameters = new NameValueCollection {
+                { "token", token },
+                { "user", user },
+                { "message", message }
+            };
+            
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    client.UploadValues("https://api.pushover.net/1/messages.json", parameters);
+                }
+            }
+            catch
+            {
+                BadRequest();
+            }
+        }
+    }
+}
